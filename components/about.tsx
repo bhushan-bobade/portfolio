55e06f01d148716d271b7bbb5ae003d429aa5d14
greatbb712@gmail.com <greatbb712@gmail.com>
2024-12-20 20:24:52 +0530

    updated about section

--- components/about.tsx
+++ components/about.tsx
@@ -14,17 +14,18 @@ export function About() {
         <h2 className="text-3xl font-bold text-center mb-8 text-white">About Me</h2>
         <div className="max-w-2xl mx-auto text-gray-300 space-y-4">
           <p>
-          Hi! I’m Bhushan Bobade, a passionate Full Stack Developer with a proven ability to craft innovative and efficient solutions in web development. 
-          As a recent graduate in Computer Science and Engineering from G.H. Raisoni College of Engineering and Management, Pune, 
-          I bring a blend of academic excellence and hands-on experience to every project.         
+            Hi! I&apos;m Bhushan Bobade, a passionate Full Stack Developer with a proven ability to craft innovative and efficient solutions in web development. 
+            As a recent graduate in Computer Science and Engineering from G.H. Raisoni College of Engineering and Management, Pune, 
+            I bring a blend of academic excellence and hands-on experience to every project.         
           </p>
           <p>
             My journey in tech began with a strong foundation in Computer Science and Engineering, 
-            and I've since expanded my expertise through hands-on experience in internships and personal projects. 
-            I'm particularly enthusiastic about technologies like React, Node.js, and Java, and I'm always eager to learn and adapt to new tools and frameworks.
+            and I&apos;ve since expanded my expertise through hands-on experience in internships and personal projects. 
+            I&apos;m particularly enthusiastic about technologies like React, Node.js, and Java, and I&apos;m always eager to learn and adapt to new tools and frameworks.
           </p>
         </div>
       </motion.div>
     </section>
   )
 }
+
